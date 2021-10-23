fa27c1ec18ba7c023009ca49af5f39b89df7a33b
Amanthapliyal <amanthapliyal2000@gmail.com>
2021-10-23 21:34:24 +0530

    server final update

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import pickle
 import socket
 
 HEADERSIZE =10
@@ -6,7 +7,7 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((socket.gethostname(),6789))
 
 while True:
-    full_msg = ''
+    full_msg = b''
     new_msg = True
     while True:
         msg = s.recv(1024)
@@ -14,10 +15,14 @@ while True:
             print(f"new message length :{msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
             new_msg = False
-        full_msg += msg.decode("utf-8")
+        full_msg += msg
         if len(full_msg)-HEADERSIZE == msglen:
-            print("fullmsg recived")
+            print("full msg recived")
             print(full_msg[HEADERSIZE:])
+
+            d=pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+
             new_msg=True
-            full_msg=''
+            full_msg=b''
     print(full_msg)
