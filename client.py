d1e5da6d1d01e3d65b76c65f07fba2959b0c8f22
Amanthapliyal <amanthapliyal2000@gmail.com>
2021-10-25 17:36:30 +0530

    client update

--- client.py
+++ client.py
@@ -1,28 +1,49 @@
-import pickle
-import socket
+import socket 
+import select
+import errno
+import sys
 
-HEADERSIZE =10
+HEADER_LENGTH = 10
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),6789))
+IP = "192.168.43.128"
+PORT = 6789
+
+my_username = input("username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode("utf-8")
+username_header = f"{len(username):< {HEADER_LENGTH}}".encode("UTF-8")
+
+client_socket.send(username_header + username)
 
 while True:
-    full_msg = b''
-    new_msg = True
-    while True:
-        msg = s.recv(1024)
-        if new_msg:
-            print(f"new message length :{msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-        full_msg += msg
-        if len(full_msg)-HEADERSIZE == msglen:
-            print("full msg recived")
-            print(full_msg[HEADERSIZE:])
+    message = input(f"{my_username}>")
+    #message = "" 
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
 
-            d=pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
-
-            new_msg=True
-            full_msg=b''
-    print(full_msg)
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("sonnection closed by the server") 
+                sys.exit()
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
+            print(f"{username}>{message}")
+    
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error', str(e))
+            sys.exit()
+        continue
+    except Exception as e:
+        print ('General Error ',str(e))
+        sys.exit()
--- server.py
+++ server.py
@@ -2,8 +2,8 @@ import socket
 import select
 
 HEADER_LENGTH = 10
-IP = "123.0.0.1"
-PORT = 1234
+IP = "192.168.43.128"
+PORT = 6789
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR ,1)
@@ -34,9 +34,11 @@ while True:
             client_socket, client_address = server_socket.accept()
 
             user = receive_message(client_socket)
-            if user in False:
+            if user in False :
                 continue
+
             sockets_list.append(client_socket)
+
             clients[client_socket] = user
             print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username :{user['data'].decode('utf-8')}")
         
